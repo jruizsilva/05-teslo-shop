dff9b597728471e3eac3ae60013e60b40bc00509
jruizsilva <ruizsilvajonathan@gmail.com>
2023-02-12 00:42:06 -0300

    popover transition added

--- components/ui/Header.tsx
+++ components/ui/Header.tsx
@@ -7,11 +7,12 @@ import {
   UsersIcon,
   UserCircleIcon
 } from '@heroicons/react/24/outline'
-import { Popover } from '@headlessui/react'
+import { Popover, Transition } from '@headlessui/react'
 import { CiLogout, CiLogin } from 'react-icons/ci'
 import { MdChildCare } from 'react-icons/md'
 import { FcBusinessman, FcBusinesswoman } from 'react-icons/fc'
 import { AiOutlineInbox } from 'react-icons/ai'
+import { Fragment } from 'react'
 
 interface Props {}
 
@@ -38,78 +39,87 @@ export function Header(props: Props) {
                 Menu
               </Popover.Button>
             </div>
-            <Popover.Panel className='bg-white absolute right-0 top-2 container max-w-xs text-gray-500 rounded-lg shadow-lg ring-1 ring-black ring-opacity-5 z-10'>
-              <div className='divide-y-2 divide-gray-50'>
-                {/* Menu header */}
-                <div className='flex justify-between items-center p-4'>
-                  <h3 className='ml-3 text-base font-medium text-gray-900'>
-                    Teslo Shop
-                  </h3>
-                  <Popover.Button className='text-base font-medium text-gray-500 hover:text-gray-900 hover:bg-gray-100 p-2 rounded-md'>
-                    <XMarkIcon className='w-6 h-6 text-gray-500 ' />
-                  </Popover.Button>
-                </div>
-                {/* Menu */}
-                <div className='grid py-1'>
-                  <MenuItem
-                    label={'Perfil'}
-                    href={''}
-                    icon={UserCircleIcon}
-                  />
-                  <MenuItem
-                    label={'Mis ordenes'}
-                    href={''}
-                    icon={ListBulletIcon}
-                  />
-                  <div className='md:hidden'>
+            <Transition
+              as={Fragment}
+              enter='transition duration-100 ease-out'
+              enterFrom='transform scale-95 opacity-0'
+              enterTo='transform scale-100 opacity-100'
+              leave='transition duration-75 ease-out'
+              leaveFrom='transform scale-100 opacity-100'
+              leaveTo='transform scale-95 opacity-0'
+            >
+              <Popover.Panel className='bg-white absolute right-0 top-2 container max-w-xs text-gray-500 rounded-lg shadow-lg ring-1 ring-black ring-opacity-5 z-10'>
+                <div className='divide-y-2 divide-gray-50'>
+                  {/* Menu header */}
+                  <div className='flex justify-between items-center p-4'>
+                    <h3 className='ml-3 text-base font-medium text-gray-900'>
+                      Teslo Shop
+                    </h3>
+                    <Popover.Button className='text-base font-medium text-gray-500 hover:text-gray-900 hover:bg-gray-100 p-2 rounded-md'>
+                      <XMarkIcon className='w-6 h-6 text-gray-500 ' />
+                    </Popover.Button>
+                  </div>
+                  {/* Menu */}
+                  <div className='grid py-1'>
                     <MenuItem
-                      label={'Hombres'}
+                      label={'Perfil'}
                       href={''}
-                      icon={FcBusinessman}
+                      icon={UserCircleIcon}
                     />
                     <MenuItem
-                      label={'Mujeres'}
+                      label={'Mis ordenes'}
                       href={''}
-                      icon={FcBusinesswoman}
+                      icon={ListBulletIcon}
+                    />
+                    <div className='md:hidden'>
+                      <MenuItem
+                        label={'Hombres'}
+                        href={''}
+                        icon={FcBusinessman}
+                      />
+                      <MenuItem
+                        label={'Mujeres'}
+                        href={''}
+                        icon={FcBusinesswoman}
+                      />
+                      <MenuItem
+                        label={'Niños'}
+                        href={''}
+                        icon={MdChildCare}
+                      />
+                    </div>
+                    <MenuItem
+                      label={'Ingresar'}
+                      href={''}
+                      icon={CiLogin}
                     />
                     <MenuItem
-                      label={'Niños'}
+                      label={'Salir'}
                       href={''}
-                      icon={MdChildCare}
+                      icon={CiLogout}
                     />
                   </div>
-                  <MenuItem
-                    label={'Ingresar'}
-                    href={''}
-                    icon={CiLogin}
-                  />
-                  <MenuItem
-                    label={'Salir'}
-                    href={''}
-                    icon={CiLogout}
-                  />
+                  {/* Admin menu */}
+                  <div className='grid py-1'>
+                    <MenuItem
+                      label={'Productos'}
+                      href={''}
+                      icon={AiOutlineInbox}
+                    />
+                    <MenuItem
+                      label={'Ordenes'}
+                      href={''}
+                      icon={ListBulletIcon}
+                    />
+                    <MenuItem
+                      label={'Usuarios'}
+                      href={''}
+                      icon={UsersIcon}
+                    />
+                  </div>
                 </div>
-
-                {/* Admin menu */}
-                <div className='grid py-1'>
-                  <MenuItem
-                    label={'Productos'}
-                    href={''}
-                    icon={AiOutlineInbox}
-                  />
-                  <MenuItem
-                    label={'Ordenes'}
-                    href={''}
-                    icon={ListBulletIcon}
-                  />
-                  <MenuItem
-                    label={'Usuarios'}
-                    href={''}
-                    icon={UsersIcon}
-                  />
-                </div>
-              </div>
-            </Popover.Panel>
+              </Popover.Panel>
+            </Transition>
           </nav>
         </header>
       </Popover>
