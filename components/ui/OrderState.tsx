d3725fa1a0445233bb38814b5d297d426d06eae1
jruizsilva <ruizsilvajonathan@gmail.com>
2023-03-16 14:27:56 -0300

    OrderState componented added

--- components/ui/OrderState.tsx
+++ components/ui/OrderState.tsx
@@ -1,16 +1,26 @@
-import { MdCreditScore } from 'react-icons/md'
+import { memo } from 'react'
+import { MdCreditScore, MdCreditCardOff } from 'react-icons/md'
 
-interface Props {}
+interface Props {
+  paid?: boolean
+}
 
-export function OrderState(props: Props) {
+export const OrderState = memo(function OrderState({
+  paid = false
+}: Props) {
+  const borderStyle = paid ? 'border-green-700' : 'border-red-700'
+  const textStyle = paid ? 'text-green-700' : 'text-red-700'
+  const textToShow = paid ? 'Orden ya fue pagada' : 'Orden no pagada'
+  const IconToShow = paid ? MdCreditScore : MdCreditCardOff
+
   return (
-    <div className='inline-flex items-center p-2 px-3 border rounded-full border-green-700'>
+    <div
+      className={`inline-flex items-center p-2 px-3 border rounded-full ${borderStyle}`}
+    >
       <span className='pr-2'>
-        <MdCreditScore className='text-lg text-green-700' />
+        <IconToShow className={`text-lg ${textStyle}`} />
       </span>
-      <span className='text-sm text-green-700'>
-        Orden ya fue pagada
-      </span>
+      <span className={`text-sm ${textStyle}`}>{textToShow}</span>
     </div>
   )
-}
+})
