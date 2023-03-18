16e2194546c74a8acde6f8773e23a3181e90295e
jruizsilva <ruizsilvajonathan@gmail.com>
2023-03-18 11:35:02 -0300

    learning swr

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,18 @@
 import { ShopLayout } from '@/components/layouts'
 import { ProductList } from '@/components/products'
 
-export default function Home() {
+import useSWR from 'swr'
+
+const fetcher = async (...args: [key: string]) =>
+  await fetch(...args).then(async (res) => await res.json())
+
+export default function HomePage() {
+  const { data, error, isLoading } = useSWR('/api/products', fetcher)
+
+  console.log(data)
+  if (typeof error === 'object') return <div>failed to load</div>
+  if (isLoading) return <div>loading...</div>
+
   return (
     <>
       <ShopLayout
